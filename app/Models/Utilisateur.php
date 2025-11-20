69cadbbfa0f241ec1a1c509d8f1e8718c10d886d
KENMEUGNEMICHELE14 <michelekenmeugne@example.com>
2025-11-20 03:24:50 +0100

    utilisation de la table user à la place de utilisateur

--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -2,10 +2,76 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Utilisateur extends Model
+class Utilisateur extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'utilisateurs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'password',
+        'date_inscription',
+        
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'date_inscription' => 'datetime',
+        
+    ];
+    
+
+
+
+    // Ici, le hasOne veut dire que les clés sont utilisées comme clé secondaire dans une autre table
+    /**
+     * Relation avec le client
+     */
+   public function client()
+    {
+        return $this->hasOne(Client::class);
+    }
+
+    /**
+     * Relation avec l'administrateur
+     */
+   public function administrateur()
+    {
+        return $this->hasOne(Administrateur::class);
+      
+}
 }
